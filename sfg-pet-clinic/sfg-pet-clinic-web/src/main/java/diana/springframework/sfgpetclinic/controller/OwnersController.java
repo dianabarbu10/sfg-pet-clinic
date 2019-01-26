c52408d6509383d3381c5e5ad0ab6593d9f96cae
diana <diana@DESKTOP-VN1DCNC>
2019-01-26 13:57:46 +0100

    List vets and owners on index pages. Closes #15 and #16

--- sfg-pet-clinic/sfg-pet-clinic-web/src/main/java/diana/springframework/sfgpetclinic/controller/OwnersController.java
+++ sfg-pet-clinic/sfg-pet-clinic-web/src/main/java/diana/springframework/sfgpetclinic/controller/OwnersController.java
@@ -1,13 +1,20 @@
 package diana.springframework.sfgpetclinic.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import diana.springframework.sfgpetclinic.services.OwnerService;
 
 @Controller
 public class OwnersController {
+	@Autowired
+	private OwnerService ownerService;
 
 	@RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
-	public String getVets() {
+	public String getVets(Model model) {
+		model.addAttribute("owners", ownerService.findAll());
 		return "owners/index";
 	}
 }
--- sfg-pet-clinic/sfg-pet-clinic-web/src/main/java/diana/springframework/sfgpetclinic/controller/VetsController.java
+++ sfg-pet-clinic/sfg-pet-clinic-web/src/main/java/diana/springframework/sfgpetclinic/controller/VetsController.java
@@ -1,13 +1,20 @@
 package diana.springframework.sfgpetclinic.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import diana.springframework.sfgpetclinic.services.VetService;
 
 @Controller
 public class VetsController {
+	@Autowired
+	private VetService vetService;
 
 	@RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-	public String getVets() {
+	public String getVets(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
 }
