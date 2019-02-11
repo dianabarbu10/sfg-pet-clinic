f88bf5d21b11e1be98055e90c2ae8863421476f2
diana <diana@DESKTOP-VN1DCNC>
2019-02-11 04:12:09 +0100

    Create VetSpeciality Entity and associate to Vet. Closes #26

--- sfg-pet-clinic/sfg-pet-clinic-data/src/main/java/diana/springframework/sfgpetclinic/model/Specialty.java
+++ sfg-pet-clinic/sfg-pet-clinic-data/src/main/java/diana/springframework/sfgpetclinic/model/Specialty.java
@@ -5,6 +5,7 @@ import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class Vet extends Person {
-	private Set<Specialty> specialties;
+public class Specialty extends BaseEntity {
+	private String description;
+
 }
