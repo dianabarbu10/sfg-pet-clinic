d610a8920e0525a7cda2d39895d21083bd2fb2a4
diana <diana@DESKTOP-VN1DCNC>
2019-02-11 04:17:58 +0100

    Create PetTypeService. Closes #28

---
+++ sfg-pet-clinic/sfg-pet-clinic-data/src/main/java/diana/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package diana.springframework.sfgpetclinic.services;
+
+import diana.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CRUDService<PetType, Long> {
+
+}
---
+++ sfg-pet-clinic/sfg-pet-clinic-data/src/main/java/diana/springframework/sfgpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,36 @@
+package diana.springframework.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import diana.springframework.sfgpetclinic.model.PetType;
+import diana.springframework.sfgpetclinic.services.PetTypeService;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long>
+		implements
+			PetTypeService {
+	@Override
+	public Set<PetType> findAll() {
+		return super.findAll();
+	}
+	@Override
+	public PetType findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public PetType save(PetType petType) {
+		return super.save(petType);
+	}
+	@Override
+	public void delete(PetType petType) {
+		super.delete(petType);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+}
