20dcd3b01a2d5aa154292187cf888cafc40aac51
diana <diana@DESKTOP-VN1DCNC>
2019-03-11 04:56:52 +0100

    Implement base entity. Closes #34

--- sfg-pet-clinic/sfg-pet-clinic-data/src/main/java/diana/springframework/sfgpetclinic/model/BaseEntity.java
+++ sfg-pet-clinic/sfg-pet-clinic-data/src/main/java/diana/springframework/sfgpetclinic/model/BaseEntity.java
@@ -2,9 +2,17 @@ package diana.springframework.sfgpetclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
 import lombok.Data;
 
 @Data
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 }
