94f21b52254f153615fe2db1909c754a0f743899
Diana Barbu <dib@kruger.dk>
2019-03-11 17:06:34 +0100

    Create Visit map service. Closes #44

---
+++ sfg-pet-clinic/sfg-pet-clinic-data/src/main/java/diana/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package diana.springframework.sfgpetclinic.services;
+
+import diana.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CRUDService<Visit, Long> {
+
+}
