5739afba60f80e706f7adecb37a9b8c8ce78c333
diana <diana@DESKTOP-VN1DCNC>
2019-03-11 05:18:49 +0100

    Create Visit entity. Closes #37

--- sfg-pet-clinic/sfg-pet-clinic-data/src/main/java/diana/springframework/sfgpetclinic/model/Pet.java
+++ sfg-pet-clinic/sfg-pet-clinic-data/src/main/java/diana/springframework/sfgpetclinic/model/Pet.java
@@ -1,11 +1,15 @@
 package diana.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -31,4 +35,7 @@ public class Pet extends BaseEntity {
 
 	@Column(name = "name")
 	private String name;
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+	private Set<Visit> visits = new HashSet<>();
 }
--- sfg-pet-clinic/sfg-pet-clinic-data/src/main/java/diana/springframework/sfgpetclinic/model/Visit.java
+++ sfg-pet-clinic/sfg-pet-clinic-data/src/main/java/diana/springframework/sfgpetclinic/model/Visit.java
@@ -2,14 +2,30 @@ package diana.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+	@Column(name = "date")
 	private LocalDate date;
+
+	@Column(name = "description")
 	private String description;
+
+	@ManyToOne
+	@JoinColumn(name = "pet_id")
 	private Pet pet;
 }
